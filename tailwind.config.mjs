375d5d4520d7e8c43f3323c59063805559a413e2
unknown <sabidoangel.uc@gmail.com>
2026-06-28 18:07:42 +0800

    test commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#0ea5e9',
+      },
+    },
+  },
+  plugins: [],
+};
